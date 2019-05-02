d2530b74e6b904763772003ec9689820b3fcf669
Amine M. Boulouma <amine@boulouma.com>
2019-05-02 09:57:31 +0200

    Finished stay in the zone with logs

--- src/stay_in_the_zone.py
+++ src/stay_in_the_zone.py
@@ -1,38 +1,44 @@
 #!/usr/bin/env python3
 
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor import INPUT_1, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor, InfraredSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 
 # Write your program here
 
-ir = InfraredSensor() 
+ir = InfraredSensor(INPUT_4) 
 leds = Leds()
 
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
+
+# Info logging
+
+logs = open("robot.log","w")
 
 # Put the infrared sensor into proximity mode.
 ir.mode = 'IR-PROX'
 
 print("The color changes due to distance.")
 
-
 while True:   
     # Infrared sensor in proximity mode will measure distance to the closest
     # object in front of it.
 
     distance = ir.value()
-
+    logs.write('infrared_distance ' + str(distance) + '\n')
     if distance < 50:
-        Leds.set_color(Leds.LEFT, Leds.RED)
-        Leds.set_color(Leds.RIGHT, Leds.RED)
-        tank_drive.on_for_seconds(SpeedPercent(100), SpeedPercent(0), 15)
+        leds.set_color('LEFT', 'RED')
+        leds.set_color('RIGHT', 'RED')
+        tank_drive.on_for_seconds(SpeedPercent(100), SpeedPercent(0), 1.5)
+        logs.write('right_turn' + '\n')
     else:
-        Leds.set_color(Leds.LEFT, Leds.GREEN)
-        Leds.set_color(Leds.RIGHT, Leds.GREEN)
-        tank_drive.on_for_seconds(SpeedPercent(100), SpeedPercent(100), 3)
-        
+        leds.set_color('LEFT', 'GREEN')   
+        leds.set_color('RIGHT', 'GREEN')
+        tank_drive.on_for_seconds(SpeedPercent(30), SpeedPercent(30), 3)
+        logs.write('move_forward' + '\n')
+
+logs.close()
 Sound.beep()       
-Leds.set_color(Leds.LEFT, Leds.GREEN)  
+leds.set_color('LEFT', 'GREEN')  
