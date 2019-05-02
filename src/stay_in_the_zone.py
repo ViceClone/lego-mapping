1305f4897653a2f0b4001142f7129f673a2a8a77
Amine M. Boulouma <amine@boulouma.com>
2019-05-02 11:17:07 +0200

    Added robot.csv

--- src/stay_in_the_zone.py
+++ src/stay_in_the_zone.py
@@ -6,6 +6,20 @@ from ev3dev2.sensor.lego import TouchSensor, InfraredSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 
+def update_position(x,y):
+    if forward_flag:    y+=1
+    elif right_flag:    x+=1
+    elif backward_flag: y-=1
+    elif left_flag:     x-=1
+    return x,y
+
+def update_flags_on_turn(forward_flag, right_flag, backward_flag, left_flag):
+    if forward_flag:    right_flag, forward_flag =  1, 0
+    elif right_flag:    backward_flag, right_flag = 1, 0
+    elif backward_flag: left_flag, backward_flag =  1, 0
+    elif left_flag:     forward_flag, left_flag =   1, 0
+    return forward_flag, right_flag, backward_flag, left_flag
+
 # Write your program here
 
 ir = InfraredSensor(INPUT_4) 
@@ -39,29 +53,16 @@ while True:
         tank_drive.on_for_seconds(SpeedPercent(100), SpeedPercent(0), 1.3)
         forward_flag, right_flag, backward_flag, left_flag = update_flags_on_turn(forward_flag, 
                                                                 right_flag, backward_flag, left_flag)
-        x, y = updated_position(x,y)
-        logs.write(str(distance) + ', 100, 0, ' + str(x) + ', ' + str(y) + '\n')
+        x, y = update_position(x,y)
+        logs.write(str(distance) + ', 100, 0, ' + str(x) + ', ' + str(y) + '\n')    
     else:
         leds.set_color('LEFT', 'GREEN')   
         leds.set_color('RIGHT', 'GREEN')
         tank_drive.on_for_seconds(SpeedPercent(30), SpeedPercent(30), 3)
-        x, y = updated_position(x,y)
+        x, y = update_position(x,y)
         logs.write(str(distance) + ', 30, 30, ' + str(x) + ', ' + str(y) + '\n')
 
 logs.close()
 Sound.beep()       
 leds.set_color('LEFT', 'GREEN')  
 
-def updated_position(x,y):
-    if forward_flag:    y+=1
-    elif right_flag:    x+=1
-    elif backward_flag: y-=1
-    elif left_flag:     x-=1
-    return x,y
-
-def update_flags_on_turn(forward_flag, right_flag, backward_flag, left_flag):
-    if forward_flag:    right_flag, forward_flag =  1, 0
-    elif right_flag:    backward_flag, right_flag = 1, 0
-    elif backward_flag: left_flag, backward_flag =  1, 0
-    elif left_flag:     forward_flag, left_flag =   1, 0
-    return forward_flag, right_flag, backward_flag, left_flag
