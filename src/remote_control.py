cae895b29a4536d402b916eb0765bb99696caa82
Franco <Donnangelo@Francos-MacBook-Pro-3.local>
2019-05-02 10:16:25 +0200

    update remote controller

--- src/remote_control.py
+++ src/remote_control.py
@@ -9,23 +9,16 @@ from time import sleep
 
 steer_pair = MoveSteering(OUTPUT_A, OUTPUT_B)
 ir = InfraredSensor()
-prev_action=0
 # Set the remote to channel 1
 
 def top_left_channel_1_action(state):
     if state: # state is True (pressed) or False
-        if prev_action==2:
-            steer_pair.on(steering=0, speed=0)
-        else:
             steer_pair.on(steering=0, speed=40)
     else:
         steer_pair.off()
 
 def bottom_left_channel_1_action(state):
     if state:
-        if prev_action==1:
-            steer_pair.on(steering=0, speed=0)
-        else:
             steer_pair.on(steering=0, speed=-40)
     else:
         steer_pair.off()
@@ -48,6 +41,5 @@ ir.on_channel1_top_right = top_right_channel_1_action
 ir.on_channel1_bottom_right = bottom_right_channel_1_action
 
 while True:
-    prev_state=state
     ir.process()
     sleep(0.01)
