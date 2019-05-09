17e73fabf1a41a24ea010e33876cc6499fb34a36
Franco <Donnangelo@franc.local>
2019-05-09 09:52:39 +0200

    remote fixed

--- src/remote_control.py
+++ src/remote_control.py
@@ -22,44 +22,44 @@ tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 ir = InfraredSensor(INPUT_1)
 # Set the remote to channel 1
 
-end = False
-
 def top_left_channel_1_action(state):
 	print("top left button")
-	tank_drive.on_for_seconds(60,60,0.5)
+	if state:
+		tank_drive.on(60,60)
+	else:
+		tank_drive.off()
 
 def bottom_left_channel_1_action(state):
 	print("bottom left button")
-	tank_drive.on_for_seconds(-60,-60,0.5)
-	# else:
-	# 	tank_drive.off()
+	if state:
+		tank_drive.on(-60,-60)
+	else:
+		tank_drive.off()
 
 def top_right_channel_1_action(state):
 	print("top right button")
-	tank_drive.on_for_seconds(-50,50,0.5)
+	if state:
+		tank_drive.on(-50,50)
+	else:
+		tank_drive.off()
 
 def bottom_right_channel_1_action(state):
 	print("bottom right button")
-	tank_drive.on_for_seconds(50,-50,0.5)
-
-def beacon_channel_1_action(state):
-	print("beacon button")
 	if state:
-		end= True
+		tank_drive.on(50,-50)
 	else:
 		tank_drive.off()
+
 
 ir.on_channel1_top_left = top_left_channel_1_action
 ir.on_channel1_bottom_left = bottom_left_channel_1_action
 ir.on_channel1_top_right = top_right_channel_1_action
 ir.on_channel1_bottom_right = bottom_right_channel_1_action
-ir.on_channel1_beacon = beacon_channel_1_action
 
 print("ready")
-while not end: 
+while True: 
 	ir.process()
 	sleep(0.01)
-sys.exit(0)
 
 
 
